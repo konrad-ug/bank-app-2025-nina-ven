6bd50799ae55371280171637f71c9e189cba9698
Nina Gos <ngos@INF.UG.EDU.PL>
2025-10-16 13:59:22 +0200

    koniec labów

--- src/account.py
+++ src/account.py
@@ -1,5 +1,13 @@
 class Account:
-    def __init__(self, first_name, last_name, balance=0):
+    def __init__(self, first_name, last_name, pesel, balance=0.0, promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
+        self.pesel = pesel
         self.balance = balance
+        if type(pesel) != 'string' and len(self.pesel) != 11:
+            self.pesel = "Invalid"
+        self.promo_code = promo_code
+        if promo_code and len(promo_code)==7 and promo_code.startswith("PROM_"):
+            self.balance += 50
+
+
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,23 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", 200)
+        account = Account("John", "Doe", '06211304545', 200.0)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 200.0
+        assert account.pesel == "06211304545"
+
+    def test_pesel_length(self):
+        account = Account("John", "Doe", '062113045', 200.0)
+        account2 = Account("John", "Doe", '062244333113045', 200.0)
+        assert account.pesel == "Invalid"
+        assert account2.pesel == "Invalid"
+
+
+
+    def test_is_code_correct_format(self):
+        account = Account("John", "Doe", '062113045', 200.0, "AAA_aa")
         assert account.balance == 200
+
+
+
