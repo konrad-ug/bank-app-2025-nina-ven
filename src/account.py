8e6f28bfa414f0b6a398b3313b0ed08b4696c356
nina-ven <n.gos.222@studms.ug.edu.pl>
2025-10-17 15:08:45 +0200

    feature 5

--- src/account.py
+++ src/account.py
@@ -4,10 +4,19 @@ class Account:
         self.last_name = last_name
         self.pesel = pesel
         self.balance = balance
-        if type(pesel) != 'string' and len(self.pesel) != 11:
+        if type(pesel) != "string" and len(self.pesel) != 11:
             self.pesel = "Invalid"
         self.promo_code = promo_code
-        if promo_code and len(promo_code)==8 and promo_code.startswith("PROM_"):
+        if promo_code and len(promo_code)==8 and promo_code.startswith("PROM_") and yob_from_pesel(self.pesel)>1960:
             self.balance += 50
 
 
+
+def yob_from_pesel(pesel): # dla roku 1900+
+    if pesel.isdigit() : # isinstance(pesel, str) możnaby ale pesel jest już sprawdzany czy jest stringiem o długości 11
+        if int(pesel[2:4]) > 12:
+            return 2000 + int(pesel[:2])
+        else:
+            return 1900 + int(pesel[:2])
+    else:
+        return 0
